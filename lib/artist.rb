0257cd4d79e82ef84a3477cd36a252965ea082cf
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-10 03:43:29 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+def self.all
+@@all
+end
+
+def new_song(name, genre)
+Song.new(name, self, genre)
+end
+
+def songs
+  Song.all.collect {|songs| songs.artist == self}
+end
+
+
+def genres
+  songs.map {|genres| genres.genre}
+end
+
+
+
+
+
+
+
+
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+@@all << self
+end
+
+def self.all
+  @@all
+end
+
+def songs
+Song.all.collect {|songs| songs.genre == self}
+end
+
+def artists
+  songs.map {|artists| artists.artist}
+end
+
+
+end
