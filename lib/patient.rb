0ba036262663f41ac3f9a2142d6fd319311bb811
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-10 04:09:58 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,30 +1,27 @@
 class Patient
 
-attr_accessor :name, :appointment, :doctor
+   attr_accessor :name, :appointment, :doctor
+  @@all = []
 
-@@all = []
+   def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def initialize(name)
-  @name = name
-@@all << self
-end
+   def self.all
+    @@all
+  end
 
-def self.all
-  @@all
-end
+   def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
 
-def new_appointment(doctor, date)
-  Appointment.new(self,doctor,date)
-end
+   def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
 
-def appointments
-Appointment.all.select {|appointment| appointment.patient == self}
-end
+   def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
 
-def doctors
-appointments.collect {|appointment| appointment.doctor}
-end
-
-
-
-end
+ end
